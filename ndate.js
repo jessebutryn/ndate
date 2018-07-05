2706bb25464623fa30c36d2faf40dd45188f3b1e
Jesse Butryn <jesse.butryn@joyent.com>
2018-07-05 14:41:54 -0600

    added offset functionality

--- ndate.js
+++ ndate.js
@@ -2,19 +2,26 @@
 
 const moment = require('moment');
 
-const {formatDate, diffDates, checkDate} = require('./lib/format');
+const {formatDate, checkDate, getOffset, deltaDates} = require('./lib/format');
 
 var argv = process.argv.slice(2);
 
 if (argv.length === 0) {
 	formatDate('now', moment());
 } else if (argv.length === 1) {
-	var dateOne = checkDate(argv[0]);
-	formatDate('time 1', dateOne);
+	if (argv[0].charAt(0) === '-' || argv[0].charAt(0) === '+') {
+		getOffset(moment(), argv[0]);
+	} else {
+		var dateOne = checkDate(argv[0]);
+		formatDate('time 1', dateOne);
+	}
 } else if (argv.length === 2) {
-	var dateOne = checkDate(argv[0]);
-	var dateTwo = checkDate(argv[1]);
-	formatDate('time 1', dateOne);
-	formatDate('time 2', dateTwo);
-	diffDates(dateOne, dateTwo);
+	if (argv[1].charAt(0) === '-' || argv[1].charAt(0) === '+') {
+		d1 = checkDate(argv[0]);
+		getOffset(moment(d1), argv[1]);
+	} else {
+		var d1 = checkDate(argv[0]);
+		var d2 = checkDate(argv[1]);
+		deltaDates(d1, d2);
+	}
 }
